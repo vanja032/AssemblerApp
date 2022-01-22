a3b1cd9755f0a4e83d1eb60732f21ed3b5ea8bc7
vanja032 <sretenovic.vanja32@gmail.com>
2022-01-22 22:03:03 +0100

    Asembler App Commit 0

---
+++ AssemblerApp/AssemblerApp/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace AssemblerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2});
+            this.listView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listView1.Location = new System.Drawing.Point(63, 142);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(283, 412);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Assembler Language Instructions";
+            this.columnHeader2.Width = 280;
+            // 
+            // listView2
+            // 
+            this.listView2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listView2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.listView2.FullRowSelect = true;
+            this.listView2.GridLines = true;
+            this.listView2.HideSelection = false;
+            this.listView2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listView2.Location = new System.Drawing.Point(454, 142);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(283, 412);
+            this.listView2.TabIndex = 6;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Machine Language Instructions";
+            this.columnHeader1.Width = 280;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = global::AssemblerApp.Properties.Resources.SaveFileIcon;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Enabled = false;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(266, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 80);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::AssemblerApp.Properties.Resources.ProcessFileIcon;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Enabled = false;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(165, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 80);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::AssemblerApp.Properties.Resources.OpenFileIcon;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(63, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 80);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 613);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Assembler";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ AssemblerApp/AssemblerApp/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AssemblerApp
+{
+    public partial class Form1 : Form
+    {
+        List<string> instructionsRead = new List<string>();
+        List<string> instructionsSave = new List<string>();
+        Dictionary<int, int> relationships = new Dictionary<int, int>();
+        Dictionary<string, int> predefinedValues = new Dictionary<string, int>();
+        Dictionary<string, string> predefinedBinaryCompValues = new Dictionary<string, string>();
+        Dictionary<string, string> predefinedBinaryDestinationValues = new Dictionary<string, string>();
+        Dictionary<string, string> predefinedBinaryJumpValues = new Dictionary<string, string>();
+        private void setPredefinedValues()
+        {
+            predefinedValues = new Dictionary<string, int>()
+            {
+                {"R0", 0 },
+                {"R1", 1 },
+                {"R2", 2 },
+                {"R3", 3 },
+                {"R4", 4 },
+                {"R5", 5 },
+                {"R6", 6 },
+                {"R7", 7 },
+                {"R8", 8 },
+                {"R9", 9 },
+                {"R10", 10 },
+                {"R11", 11 },
+                {"R12", 12 },
+                {"R13", 13 },
+                {"R14", 14 },
+                {"R15", 15 },
+                {"SP", 0 },
+                {"LCL", 1 },
+                {"ARG", 2 },
+                {"THIS", 3 },
+                {"THAT", 4 },
+                {"SCREEN", 16384 },
+                {"KBD", 24576 }
+            };
+            predefinedBinaryCompValues = new Dictionary<string, string>()
+            {
+                {"0", "0101010" },
+                {"1", "0111111" },
+                {"-1", "0111010" },
+                {"D", "0001100" },
+                {"A", "0110000" },
+                {"!D", "0001101" },
+                {"!A", "0110001" },
+                {"-D", "0001111" },
+                {"-A", "0110011" },
+                {"D+1", "0011111" },
+                {"1+D", "0011111" },
+                {"A+1", "0110111" },
+                {"1+A", "0110111" },
+                {"D-1", "0001110" },
+                {"-1+D", "0001110" },
+                {"A-1", "0110010" },
+                {"-1+A", "0110010" },
+                {"D+A", "0000010" },
+                {"A+D", "0000010" },
+                {"D-A", "0010011" },
+                {"-A+D", "0010011" },
+                {"A-D", "0000111" },
+                {"-D+A", "0000111" },
+                {"D&A", "0000000" },
+                {"A&D", "0000000" },
+                {"D|A", "0010101" },
+                {"A|D", "0010101" },
+                {"M", "1110000" },
+                {"!M", "1110001" },
+                {"-M", "1110011" },
+                {"M+1", "1110111" },
+                {"1+M", "1110111" },
+                {"M-1", "1110010" },
+                {"-1+M", "1110010" },
+                {"D+M", "1000010" },
+                {"M+D", "1000010" },
+                {"D-M", "1010011" },
+                {"-M+D", "1010011" },
+                {"M-D", "1000111" },
+                {"-D+M", "1000111" },
+                {"D&M", "1000000" },
+                {"M&D", "1000000" },
+                {"M|D", "1010101" },
+                {"D|M", "1010101" }
+            };
+            predefinedBinaryDestinationValues = new Dictionary<string, string>()
+            {
+                {"null", "000" },
+                {"M", "001" },
+                {"D", "010" },
+                {"DM", "011" },
+                {"A", "100" },
+                {"AM", "101" },
+                {"AD", "110" },
+                {"ADM", "111" }
+            };
+            predefinedBinaryJumpValues = new Dictionary<string, string>()
+            {
+                {"null", "000" },
+                {"JGT", "001" },
+                {"JEQ", "010" },
+                {"JGE", "011" },
+                {"JLT", "100" },
+                {"JNE", "101" },
+                {"JLE", "110" },
+                {"JMP", "111" }
+            };
+        }
+        private void clearDefaultValues()
+        {
+            predefinedValues.Clear();
+            predefinedBinaryCompValues.Clear();
+            predefinedBinaryDestinationValues.Clear();
+            predefinedBinaryJumpValues.Clear();
+            instructionsRead.Clear();
+            instructionsSave.Clear();
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Title = "Open Assembly Instructions File";
+                dialog.Filter = "ASM Files (*.asm)|*.asm";
+                
+                if(dialog.ShowDialog() == DialogResult.OK)
+                {
+                    clearDefaultValues();
+                    setPredefinedValues();
+                    StreamReader reader = new StreamReader(dialog.FileName);
+                    while(!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine().Replace(" ", string.Empty).ToUpper();
+                        instructionsRead.Add(line);
+                        listView1.Items.Add(new ListViewItem(line));
+                    }
+                    reader.Dispose();
+                    reader.Close();
+                    button2.Enabled = true;
+                    relationships.Clear();
+                    MessageBox.Show("Fajl je ispravno učitan.");
+                }
+                else
+                    MessageBox.Show("Neuspešno čitanje fajla!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Dictionary<string, int> customDefinedLabels = new Dictionary<string, int>();
+                List<string> temp = new List<string>();
+                int index = 0;
+                int indexTemp = -1;
+                foreach (string instruction in instructionsRead)
+                {
+                    indexTemp++;
+                    string newInstruction = instruction;
+                    if (instruction.Contains("//"))
+                        newInstruction = instruction.Substring(0, instruction.IndexOf("//"));
+
+                    if (newInstruction == "")
+                        continue;
+                    else if (newInstruction.IndexOf('(') == 0 && newInstruction.IndexOf(')') == newInstruction.Length - 1)
+                    { customDefinedLabels.Add(newInstruction.Trim('(', ')'), index); continue; }
+                    temp.Add(newInstruction);
+                    relationships.Add(indexTemp, index);
+                    index++;
+                }
+                int variableIndex = 16;
+                foreach (string instruction in temp)
+                {
+                    if (instruction.IndexOf('@') != -1)
+                    {
+                        if (int.TryParse(instruction.Trim('@'), out int number))
+                        {
+                            if (number >= 0)
+                            { instructionsSave.Add("0" + Convert.ToString(number, 2).PadLeft(15, '0')); }
+                        }
+                        else if (customDefinedLabels.Keys.Any(instruction.Trim('@').Contains))
+                        { instructionsSave.Add("0" + Convert.ToString(customDefinedLabels[instruction.Trim('@')], 2).PadLeft(15, '0')); }
+                        else if (predefinedValues.Keys.Any(instruction.Trim('@').Contains))
+                        { instructionsSave.Add("0" + Convert.ToString(predefinedValues[instruction.Trim('@')], 2).PadLeft(15, '0')); }
+                        else
+                        {
+                            instructionsSave.Add("0" + Convert.ToString(variableIndex, 2).PadLeft(15, '0'));
+                            predefinedValues.Add(instruction.Trim('@'), variableIndex);
+                            variableIndex++;
+                        }
+                    }
+                    else
+                    {
+                        if (instruction.IndexOf('=') != -1)
+                        {
+                            instructionsSave.Add("111" + predefinedBinaryCompValues[instruction.Split('=')[1]] + predefinedBinaryDestinationValues[instruction.Split('=')[0]] + predefinedBinaryJumpValues["null"]);
+                        }
+                        else if (instruction.IndexOf(';') != -1)
+                        {
+                            instructionsSave.Add("111" + predefinedBinaryCompValues[instruction.Split(';')[0]] + predefinedBinaryDestinationValues["null"] + predefinedBinaryJumpValues[instruction.Split(';')[1]]);
+                        }
+                    }
+                }
+                foreach (string instruction in instructionsSave)
+                {
+                    listView2.Items.Add(new ListViewItem(instruction));
+                }
+                button3.Enabled = true;
+                MessageBox.Show("Instrukcije su ispravno prevedene u mašinski kod.");
+            }
+            catch
+            {
+                MessageBox.Show("Instrukcije nisu ispravno prevedene u mašinski kod!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog dialog = new SaveFileDialog())
+            {
+                dialog.Title = "Save HACK Instructions File";
+                dialog.Filter = "HACK Files (*.hack)|*.hack";
+
+                if (dialog.ShowDialog() == DialogResult.OK && dialog.FileName != "")
+                {
+                    StreamWriter writer = new StreamWriter(dialog.FileName);
+                    foreach (string instruction in instructionsSave)
+                    {
+                        writer.WriteLine(instruction);
+                    }
+                    writer.Dispose();
+                    writer.Close();
+                    MessageBox.Show("Uspešno sačuvan fajl.");
+                }
+                else
+                    MessageBox.Show("Potrebno je uneti ispravan naziv fajla!");
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listView2.SelectedIndices.Clear();
+            foreach (ListViewItem item in listView1.Items)
+                item.BackColor = Color.DarkSeaGreen;
+            foreach (ListViewItem item in listView2.Items)
+                item.BackColor = Color.DarkSeaGreen;
+            foreach (int index in listView1.SelectedIndices)
+                if (relationships.Keys.Contains(index))
+                    listView2.Items[relationships[index]].BackColor = Color.FromArgb(255, 180, 51);
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listView1.SelectedIndices.Clear();
+            foreach (ListViewItem item in listView1.Items)
+                item.BackColor = Color.DarkSeaGreen;
+            foreach (ListViewItem item in listView2.Items)
+                item.BackColor = Color.DarkSeaGreen;
+            foreach (int index in listView2.SelectedIndices)
+                if (relationships.Values.Contains(index))
+                    listView1.Items[index].BackColor = Color.FromArgb(255, 180, 51);
+        }
+    }
+}
